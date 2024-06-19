f3e963de4ecc1c364abdefc2fd21adfc828f8517
Кристина Волосова <justanobody017527@gmail.com>
2024-06-19 11:14:14 +0300

    Homework DONE

---
+++ Calculator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите выражение формата a ? b для вычисления или \"exit\" для выхода");
+			while (true)
+			{
+				string userInput = Console.ReadLine();
+				if (userInput == "exit")
+				{
+					break;
+				}
+				string[] expression = userInput.Split(new char[] { '+', '-', '/', '*' });
+				if (expression.Length != 2)
+				{
+					Console.WriteLine("Можно вычислить выражение только из двух операндов.");
+					continue;
+				}
+				double operand1, operand2;
+				//Трай парс возвращает логическое значение и сам результат парсинга, если он успешен, в какую-то переменную с помощью оператора out
+				//Трим без аргументов удаляет лишние пробелы по бокам
+				if (!double.TryParse(expression[0].Trim(), out operand1) || !double.TryParse(expression[1].Trim(), out operand2))
+				{
+                    Console.WriteLine("Невозможно выявить операнд.");
+					continue;
+                }
+				//Тут будет либо первый встреченный знак из массива, либо -1, который упадет в дефолт в свитче
+				char op = userInput[userInput.IndexOfAny(new char[] { '+', '-', '*', '/' })];
+				switch (op) 
+				{
+					case '+':
+                        Console.WriteLine(operand1 + operand2);
+						break;
+					case '-':
+						Console.WriteLine(operand1 - operand2);
+						break;
+					case '*':
+						Console.WriteLine(operand1 * operand2);
+						break;
+					case '/':
+						if (operand2 != 0)
+						{
+							Console.WriteLine(operand1 / operand2);
+						}
+						else 
+						{
+                            Console.WriteLine("Деление на ноль невозможно.");
+                        }
+						break;
+					default:
+						Console.WriteLine("Неподдерживаемый оператор.");
+						break;
+                }
+
+
+			}
+		}
+		
+	}
+}
---
+++ Game/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+	internal class Program
+	{
+		class Square
+		{
+			private int size;
+			private ConsoleColor color;
+			private int x;
+			private int y;
+			private int checkSize(int value)
+			{
+				if (value <= 0) { value = 1; }
+				else if (value > 10) { value = 10; }
+				return value;
+				
+			}
+			//От 1 до 10
+			public void setSize(int size) { this.size = checkSize(size); }
+			public void setColor(ConsoleColor color) { this.color = color; }
+			private int checkX(int x) 
+			{ 
+				if (x <= 0) 
+				{ 
+					x = 0;
+				}
+				else if (x > (120 - (this.size * 2))) 
+				{ 
+					x = 120 - size * 2; 
+				}
+				return x; 
+			}
+			private int checkY(int y)
+			{
+				if(y < 0) 
+				{
+					y = 0;
+				}
+				else if(y > 30 - this.size)
+				{
+					y = 30 - this.size;
+				}
+				return y;
+			}
+			public void setPosition(int x, int y)
+			{
+				this.x = checkX(x);
+				this.y = checkY(y);
+			}
+			public int getX() { return x; }
+			public int getY() { return y; }
+			public int getSize() { return size; }
+			public ConsoleColor getColor() { return color; }
+			public void print()
+			{
+				Console.BackgroundColor = color;
+				Console.ForegroundColor = color;
+				for (int i = 0; i < size; i++) 
+				{ 
+					Console.SetCursorPosition(x, y + i);
+					for(int j = 0; j < size; j++) 
+					{
+                        Console.Write("  ");
+                    }
+                }
+				Console.ResetColor();
+			}
+			public void clear()
+			{
+				Console.ForegroundColor = ConsoleColor.Black;
+				for (int i = 0; i < size; i++)
+				{
+					Console.SetCursorPosition(x, y + i);
+					for (int j = 0; j < size; j++)
+					{
+						Console.Write("  ");
+					}
+					Console.WriteLine();
+				}
+				Console.ResetColor();
+			}
+			public void move()
+			{
+				ConsoleKeyInfo key;
+				do
+				{
+					print();
+					key = Console.ReadKey(true);
+					clear();
+					switch (key.Key) 
+					{
+						case ConsoleKey.UpArrow:
+						case ConsoleKey.W:
+							y = checkY(y-1); 
+							break;
+						case ConsoleKey.DownArrow:
+						case ConsoleKey.S:
+							y = checkY(y+1);
+							break;
+						case ConsoleKey.LeftArrow:
+						case ConsoleKey.A:
+							x = checkX(x-1);
+							break;
+						case ConsoleKey.RightArrow:
+						case ConsoleKey.D:
+							x = checkX(x+1);
+							break;
+						default:
+							break;
+					}
+				} while (key.Key != ConsoleKey.Escape);
+			}
+			public Square()
+			{
+				x = 0;
+				y = 0;
+				size = 5;
+				color = ConsoleColor.White;
+			}
+			~Square()
+			{
+
+			}
+		};
+		static void Main(string[] args)
+		{
+			Square square = new Square();
+			square.move();
+		}
+	}
+}
