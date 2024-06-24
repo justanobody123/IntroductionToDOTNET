50b885e49be1fab1fb043338ba621f50ee872cfb
Кристина Волосова <justanobody017527@gmail.com>
2024-06-24 20:44:20 +0300

    Class work DONE

--- Class Recursion/Program.cs
+++ Class Recursion/Program.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Numerics;
+//using System.Exception;
 namespace Class_Recursion
 {
 	internal class Program
@@ -17,13 +18,21 @@ namespace Class_Recursion
 			long n = Convert.ToInt64(Console.ReadLine());
 			try
 			{
+				DateTime start = DateTime.Now;
 				Console.WriteLine($"{n}! = {Factorial(n)}");
-			}
-			catch (Exception ex)
+				DateTime end = DateTime.Now;
+				TimeSpan duration = end - start;
+                Console.WriteLine($"Duration: {duration.ToString("g")}");
+            }
+			catch (StackOverflowException ex)
 			{
-
                 Console.WriteLine($"{ex.GetType()}: {ex.Message}");
             }
+			catch(Exception ex)
+			{
+				Console.WriteLine($"{ex.GetType()}: {ex.Message}");
+			}
+            Console.WriteLine("Calculation DONE");
         }
 		static void Elevator(int floor)
 		{
@@ -37,12 +46,27 @@ namespace Class_Recursion
             Elevator(floor - 1);
 			Console.WriteLine($"Вы на {floor} этаже.");
 		}
-		static long Factorial(long n)
+		static BigInteger Factorial(long n)
 		{
-			if (n <= 1) return 1;
-			long f = n * Factorial(n - 1);
-			Console.WriteLine($"{n}! = {f}");
+			BigInteger f = 1;
+			for (int i = 2; i <= n; i++)
+			{
+				f *= i;
+			}
 			return f;
+			//if (n <= 1) return 1;
+			//try
+			//{
+			//	BigInteger f = n * Factorial(n - 1);
+			//	return f;
+			//}
+			//catch (Exception ex)
+			//{
+
+			//	throw ex;
+			//}
+			////Console.WriteLine($"{n}! = {f}");
+
 		}
 	}
 }
